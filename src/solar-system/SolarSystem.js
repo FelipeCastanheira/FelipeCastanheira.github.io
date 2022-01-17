f9c4cd100aa8938de6418b8c21738fef6907cd8f
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-01-17 13:34:16 -0300

    CSS - Home

--- src/solar-system/SolarSystem.js
+++ src/solar-system/SolarSystem.js
@@ -3,16 +3,16 @@ import Header from './components/Header';
 import Universe from './components/Universe';
 import Missions from './components/Missions';
 import planets from './data/planets';
-import './App.css';
+import style from './style.module.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <>
+      <main className={ style.solarsystem }>
         <Header />
         <Universe planets={ planets } />
         <Missions />
-      </>
+      </main>
     );
   }
 }
