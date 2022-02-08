33383a5d5a41f3bbc14b37e0da9b714ad0a4cd0d
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-08 16:38:11 -0300

    first 3d animation

--- src/solar-system/components/Header.js
+++ src/solar-system/components/Header.js
@@ -4,7 +4,20 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>Sistema Solar</h1>
+        <h1>
+          <p>S</p>
+          <p>I</p>
+          <p>S</p>
+          <p>T</p>
+          <p>E</p>
+          <p>M</p>
+          <p>A</p>
+          <p>S</p>
+          <p>O</p>
+          <p>L</p>
+          <p>A</p>
+          <p>R</p>
+        </h1>
       </header>
     );
   }
