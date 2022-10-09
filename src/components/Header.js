dd9f14c41e5cea13d8dd296f0fce8eec675f8a6a
FelipeCastanheira <castanheirafc2@gmail.com>
2022-10-09 15:41:31 -0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,17 @@ import whiteLogoFC from '../img/logosFC/logoFc.png';
 
 export class Header extends React.Component {
   render() {
+    const { title } = this.props;
     return (
       <header className={ style.topo }>
-        <div>
+        <div className="topo-main">
           <img src={ whiteLogoFC } alt="logo branca FC" />
-          <h1>Felipe Castanheira</h1>
+          <h1>{ title || 'Felipe Castanheira' }</h1>
         </div>
         <nav>
           <h2>
             <a href="#Soft">Sobre Mim</a>
-            <a href="#projects">Portfólio</a>
+            <a href="https://wa.me/message/7TZJV2L3ADCXA1">Contato</a>
           </h2>
         </nav>
       </header>
